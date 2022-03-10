e8003875cb66e50b41837446f1303f1084ee8767
Melvin <76600771+mlvin@users.noreply.github.com>
2022-03-10 22:40:44 +0100

    Create metaverse.py

---
+++ metaverse-calendar/metaverse.py
@@ -0,0 +1,24 @@
+import math
+def solve():
+    [n_user , m] = list(map(int, input().strip().split()))
+    p = list(map(int, input().strip().split()))
+    daycount = 0
+    calendar = [0]*m
+
+    for user in p:
+        for i in range(0, m, user):
+            calendar[i] += 1
+    for i in calendar:
+        if i == n_user:
+            daycount += 1
+    return daycount
+
+        
+
+
+
+
+T = int(input())
+with open("output.txt", "w") as f:
+    for i in range(T):
+        f.write(f"Case #{i+1}: {solve()}\n")
